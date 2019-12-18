35223a2c3fca737da0207f0e7fe62c42a9729d5a
Lori Boyd <lori.p.boyd@gmail.com>
2019-12-18 07:32:43 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -48,20 +48,7 @@ def naming_system
       label: "Phylum",
       sub_category: {
         label: "Class"
-        sub_category: {
-          label: 'Order',
-          sub_category: {
-            label: 'Family',
-            sub_category: {
-              label: 'Genus',
-              sub_category: {
-                label: 'Species'
-              }
-            }
-          }
-        }
-      }
-    }
+        sub_category {}
   }
 
 end
